5780f7af68a13395950b1e82dbae3b07ad0d9fce
Josivan Pereira da Silva <josivan.engenharia@gmail.com>
2018-10-31 20:16:48 -0300

    Update IElementoGrafico.cs

--- CatDogFood_ConsoleAndGUI_by_Code/IElementoGrafico.cs
+++ CatDogFood_ConsoleAndGUI_by_Code/IElementoGrafico.cs
@@ -1,1 +1,15 @@
 
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+interface IElementoGrafico
+{
+    void Mover();
+    void Desenhar(Graphics g);
+}
+
